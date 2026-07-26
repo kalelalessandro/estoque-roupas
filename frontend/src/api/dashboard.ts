41065f50b9dd913b8e74de9c0cfc2358ef484790
Kalel Alessandro <kalelalessandrooficial@gmail.com>
2026-07-26 15:15:57 -0300

    Delete frontend directory

--- frontend/src/api/dashboard.ts
+++
@@ -1,6 +0,0 @@
-import { DashboardSummary } from '../types';
-import { api } from './client';
-
-export const dashboardApi = {
-  summary: () => api.get<DashboardSummary>('/dashboard/summary').then((r) => r.data),
-};
--- frontend/src/api/sales.ts
+++
@@ -1,9 +0,0 @@
-import { Sale } from '../types';
-import { api } from './client';
-
-export const salesApi = {
-  list: () => api.get<Sale[]>('/sales').then((r) => r.data),
-
-  create: (productId: string, quantity: number) =>
-    api.post<Sale>('/sales', { productId, quantity }).then((r) => r.data),
-};
